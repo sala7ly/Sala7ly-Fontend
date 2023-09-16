cadce571b4c63dbc63bf2297a6c417294c5c1beb
Nabil Mohamed <106386970+N0ble-dev@users.noreply.github.com>
2023-09-16 17:49:06 +0300

    Update Landing.jsx
    add navigate to move to another component

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import landing from '../../assests/imgs/landing.svg'
-import logoShape from '../../assests/imgs/logoshape.png'
-import { Button } from '@material-tailwind/react';
+import React from "react";
+import landing from "../../assests/imgs/landing.svg";
+import logoShape from "../../assests/imgs/logoshape.png";
+import { Button } from "@material-tailwind/react";
+import {useNavigate} from "react-router-dom"
 const Landing = () =>
 {
+    const navigate = useNavigate()
+    const handleClick = () =>
+    {
+        navigate("/signup")
+    }
     return (
-        <div className='bg-orange-50 relative  min-h-screen overflow-hidden'>
-            <div className='container mx-auto md:mx-7 flex flex-col md:flex-row justify-between items-center text-green-50 md:pt-10 pt-5'>
-                <div className='pb-4 flex flex-col gap-6  text-center md:text-left'>
-                    <h2 className=' font-bold md:text-5xl text-3xl'>راحتك وسعادتك تهمنا عشان كدا وفرنالك صلحلى</h2>
-                    <p className='text-lg'>وفرنالك مجموعه من العمال المتخصصين فى مجالهم  وتقدر تتواصل معام بسهوله</p>
-                    <div className='flex '>
-                        <Button
+        <div className="bg-orange-50 relative  min-h-screen overflow-hidden">
+            <div className="container mx-auto md:mx-7 flex flex-col md:flex-row justify-between items-center text-green-50 md:pt-10 pt-5">
+                <div className="pb-4 flex flex-col gap-6  text-center md:text-left">
+                    <h2 className=" font-bold md:text-5xl text-3xl">
+                        راحتك وسعادتك تهمنا عشان كدا وفرنالك صلحلى
+                    </h2>
+                    <p className="text-lg">
+                        وفرنالك مجموعه من العمال المتخصصين فى مجالهم وتقدر تتواصل معام
+                        بسهوله
+                    </p>
+                    <div className="flex ">
+                        <Button onClick={handleClick}
                             size="lg"
                             variant="gradient"
-                            color='blue-gray'
+                            color="blue-gray"
                             className="group relative flex items-center gap-3 overflow-hidden pr-[72px] m-auto md:m-0"
                         >
                             سجل معانا دلوقتي
@@ -25,14 +36,12 @@ const Landing = () =>
                     </div>
                 </div>
 
-                <img src={landing} className=' h-96 relative z-10' alt="" />
-
+                <img src={landing} className=" h-96 relative z-10" alt="" />
             </div>
 
-            <div className='ball-one'></div>
+            <div className="ball-one"></div>
         </div>
-
     );
-}
+};
 
 export default Landing;
